80d435185a69ed5c8cf18a80b6cfb41602d5df54
INwafor <inwafor@uoguelph.ca>
2023-07-05 11:49:10 -0400

    edited names of plates

--- R-scripts/02-data-import-IN.R
+++ R-scripts/02-data-import-IN.R
@@ -17,17 +17,17 @@ theme_set(theme_cowplot())
 
 ## TESTER : JUNE 16, 42C
 ##read in plate
-plate <- read_excel("data-raw/June1623_42C.xlsx", range = "A40:CL137")
+june16_42 <- read_excel("data-raw/June1623_42C.xlsx", range = "A40:CL137")
 
 ##filter table to time in seconds, temperature and OD value
-plate2 <- plate %>% 
+june16_42 <- june16_42 %>% 
   filter(`Time [s]` != "Temp. [°C]") %>% 
   gather(2:90, key = time, value = OD600) %>% 
   rename(well = `Time [s]`) %>% 
   mutate(time = as.numeric(time))
 
 ## Assign treatments to wells
-plate3 <- plate2 %>% 
+june16_42 <- june16_42 %>% 
   mutate(treatment = case_when(str_detect(well, "A") ~ "WT",
                                str_detect(well, "B") ~ "FLZ 1",
                                str_detect(well, "C") ~ "FLZ 2",
@@ -39,66 +39,66 @@ plate3 <- plate2 %>%
                                TRUE ~ "water"))
 
 ##plot of wells together in facets by treatment
-plate3 %>% 
+june16_42 %>% 
   ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line() +  ## can remove the plus and next line to plot in one graph!
   facet_wrap( ~ treatment)
 
 
 ### June 28th Growth Curves 
 ## 30 Degrees C
-plate_j28_30 <- read_excel("data-raw/June2823_30C.xlsx", range = "A40:CL137")
+june28_30 <- read_excel("data-raw/June2823_30C.xlsx", range = "A40:CL137")
 
 #filter
-plate_j28_30_2 <- plate_j28_30 %>%
+june28_30 <- june28_30 %>%
   filter(`Time [s]` != "Temp. [°C]") %>% 
   gather(2:90, key = time, value = OD600) %>% 
   rename(well = `Time [s]`) %>% 
   mutate(time = as.numeric(time))
 
-plate_j28_30_3 <- plate_j28_30_2 %>% 
+june28_30 <- june28_30 %>% 
   mutate(treatment = case_when(str_detect(well, "A") ~ "fRS585",
                                str_detect(well, "B") ~ "Blank",
                                TRUE ~ "water"))
 
-plate_j28_30_3 %>% 
-  ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line()+
+june28_30 %>% 
+  ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line() +
   ggtitle("June 28th, 30C")
 
 #34 Degrees C
-plate_j28_34 <- read_excel("data-raw/June28_34C.xlsx", range = "A40:CL137")
+june28_34 <- read_excel("data-raw/June28_34C.xlsx", range = "A40:CL137")
 
-plate_j28_34_2 <- plate_j28_34 %>%
+june28_34 <- june28_34 %>%
   filter(`Time [s]` != "Temp. [°C]") %>% 
   gather(2:90, key = time, value = OD600) %>% 
   rename(well = `Time [s]`) %>% 
   mutate(time = as.numeric(time))
 
-plate_j28_34_3 <- plate_j28_34_2 %>% 
+june28_34 <- june28_34 %>% 
   mutate(treatment = case_when(str_detect(well, "A") ~ "fRS585",
                                str_detect(well, "B") ~ "Blank",
                                TRUE ~ "water"))
 
-plate_j28_34_3 %>% 
-  ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line()+
+june28_34 %>% 
+  ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line() +
   ggtitle("June 28th, 34C")
 ## nothing was contaminated - but weird bump in water
 
 ##June 29th 
 #37 C
-plate_j29_37 <- read_excel("data-raw/June2923_37C.xlsx", range = "A40:CL137")
+june29_37 <- read_excel("data-raw/June2923_37C.xlsx", range = "A40:CL137")
 
-plate_j29_37 <- plate_j29_37 %>%
+june29_37 <- june29_37 %>%
   filter(`Time [s]` != "Temp. [°C]") %>% 
   gather(2:90, key = time, value = OD600) %>% 
   rename(well = `Time [s]`) %>% 
   mutate(time = as.numeric(time))
 
-plate_j29_37 <- plate_j29_37 %>%
+june29_37 <- june29_37 %>%
   mutate(treatment = case_when(str_detect(well, "A") ~ "fRS585",
                                str_detect(well, "B") ~ "Blank",
                                TRUE ~ "water"))
 
-plate_j29_37 %>%
+june29_37 %>%
   ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line() +
   ggtitle("June 29th, 37C")
 ## blanks contaminated
@@ -106,20 +106,20 @@ plate_j29_37 %>%
 
 ##JUNE 30TH
 ##41 C (labelled as the 29th but it is june 30th)
-plate_j29_41 <- read_excel("data-raw/June2923_41C.xlsx", range = "A40:CL137")
+june29_41 <- read_excel("data-raw/June2923_41C.xlsx", range = "A40:CL137")
 
-plate_j29_41 <- plate_j29_41 %>%
+june29_41 <- june29_41 %>%
   filter(`Time [s]` != "Temp. [°C]") %>% 
   gather(2:90, key = time, value = OD600) %>% 
   rename(well = `Time [s]`) %>% 
   mutate(time = as.numeric(time))
 
-plate_j29_41 <- plate_j29_41 %>%
+june29_41 <- june29_41 %>%
   mutate(treatment = case_when(str_detect(well, "A") ~ "fRS585",
                                str_detect(well, "B") ~ "Blank",
                                TRUE ~ "water"))
 
-plate_j29_41 %>%
+june29_41 %>%
   ggplot(aes(x = time, y = OD600, group = well, color = treatment)) + geom_line() +
   ggtitle("June 30th, 41C")
 ## whats going on with the water on this one > 41C is where it did because it was mostly at 41.5C 
