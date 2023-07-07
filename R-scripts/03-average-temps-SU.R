5cfdfe32dd6cd416e114485ceb2778ecb018b8d3
suzhova <jane@example.com>
2023-07-07 11:11:55 -0400

    avg temp june30_40deg

--- R-scripts/03-average-temps-SU.R
+++ R-scripts/03-average-temps-SU.R
@@ -1,2 +1,23 @@
 #SVETA
 #GROWTH CURVE AVG ACTUAL TEMPS
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(janitor)
+install.packages("cowplot")
+library(cowplot) #this package is an add-on to ggplot which automatically gives a theme
+
+###reading in June 30, 40 deg
+#couldn't find a way to exclude the temp row when doing mean
+#so ended up just selecting from B onwards when pulling in data
+june30_40 <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/June3023_40C.xlsx", range = "B40:CL137") %>%
+  clean_names() %>% 
+  view()
+
+###avg temp
+june30_40 %>% 
+  top_n(n = 1) %>% 
+  t() %>% 
+  mean()
+
