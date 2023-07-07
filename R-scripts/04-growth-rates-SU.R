a2336aa4414235c40f7564899fde811371c21e60
suzhova <jane@example.com>
2023-07-07 14:39:25 -0400

    copied joey's code

---
+++ R-scripts/04-growth-rates-SU.R
@@ -0,0 +1,58 @@
+#SVETA
+#GROWTH RATES
+
+###CODE FROM JOEY (SLACK)
+### plotting growth curves from yeast data
+
+library(tidyverse)
+library(readxl)
+library(cowplot)
+theme_set(theme_cowplot())
+
+
+plate <- read_excel("data-raw/June1623_30C.xlsx", range = "A40:CL137")
+
+plate2 <- plate %>% 
+  filter(`Time [s]` != "Temp. [°C]") %>% 
+  gather(2:90, key = time, value = OD) %>% 
+  rename(well = `Time [s]`) %>% 
+  mutate(time = as.numeric(time))
+
+
+### this is one way of assigning treatments to wells. Alternatively, you could list out all the wells in an csv or xls file with their treatments and then do a left join. This would be my preferred appraoach
+plate3 <- plate2 %>% 
+  mutate(treatment = case_when(str_detect(well, "A") ~ "wt",
+                               str_detect(well, "B") ~ "ftz1",
+                               str_detect(well, "C") ~ "ftz2",
+                               str_detect(well, "D") ~ "ftz3",
+                               str_detect(well, "E") ~ "casp1",
+                               str_detect(well, "F") ~ "casp2",
+                               str_detect(well, "G") ~ "casp3",
+                               str_detect(well, "H1|H2|H3") ~ "blank",
+                               TRUE ~ "water")) ### this is a shortcut to assign all the remaining wells to water
+
+## plot them all together
+plate3 %>% 
+  ggplot(aes(x = time, y = OD, group = well, color = treatment)) + geom_line() +
+  
+  ## plot in facets
+  plate3 %>% 
+  ggplot(aes(x = time, y = OD, group = well, color = treatment)) + geom_line() +
+  facet_wrap( ~ treatment)
+
+
+### estimate growth rates
+library(devtools)
+# install_github("ctkremer/mleTools") ## leaving these here so you can see how to install them
+# install_github("ctkremer/growthTools")
+library(growthTools)
+
+
+b1 <- plate3 %>% 
+  filter(well == "B2") %>% 
+  mutate(log_od = log(OD)) %>% 
+  mutate(time_days = time / 86400) ### to get time in days
+
+res<-get.growth.rate(b1$time_days,b1$log_od,plot.best.Q = TRUE,id = 'Population A')
+res$best.model
+res$best.slope
