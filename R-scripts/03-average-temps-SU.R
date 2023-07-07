fb7a1f6d23696fbaa3292f1f25d3b48d62c77567
suzhova <jane@example.com>
2023-07-07 11:23:25 -0400

    more avg temps

--- R-scripts/03-average-temps-SU.R
+++ R-scripts/03-average-temps-SU.R
@@ -20,4 +20,38 @@ june30_40 %>%
   top_n(n = 1) %>% 
   t() %>% 
   mean()
+#answer: 40.04607
+#checking in excel: 40.04606742
 
+###June 30, 41 deg
+june30_41 <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/June2923_41C.xlsx", range = "B40:CL137") %>% 
+  clean_names() %>% 
+  view()
+
+june30_41 %>% 
+  top_n(n = 1) %>% 
+  t() %>% 
+  mean()
+#answer: 41.1809
+
+###June 29, 37 deg
+june29_37 <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/June2923_37C.xlsx", range = "B40:CL137") %>%
+  clean_names() %>% 
+  view()
+
+june29_37 %>% 
+  top_n(n = 1) %>% 
+  t() %>% 
+  mean()
+#answer: 37.1191
+
+###June 28, 30 deg
+june28_30 <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/June2823_30C.xlsx", range = "B40:CL137") %>% 
+  clean_names() %>% 
+  view()
+
+june28_30 %>% 
+  top_n(n = 1) %>% 
+  t() %>% 
+  mean()
+#answer: 30.44719
