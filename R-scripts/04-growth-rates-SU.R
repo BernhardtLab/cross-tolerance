f8ddb8d6a1545283fd530a3fe74086d1ba15804a
suzhova <jane@example.com>
2023-07-07 15:04:43 -0400

    attempted growth rate for june 30 at 40 deg

--- R-scripts/04-growth-rates-SU.R
+++ R-scripts/04-growth-rates-SU.R
@@ -17,7 +17,8 @@ plate2 <- plate %>%
   filter(`Time [s]` != "Temp. [°C]") %>% 
   gather(2:90, key = time, value = OD) %>% 
   rename(well = `Time [s]`) %>% 
-  mutate(time = as.numeric(time))
+  mutate(time = as.numeric(time)) %>% 
+  view
 
 
 ### this is one way of assigning treatments to wells. Alternatively, you could list out all the wells in an csv or xls file with their treatments and then do a left join. This would be my preferred appraoach
@@ -57,3 +58,30 @@ b1
 res<-get.growth.rate(b1$time_days,b1$log_od,plot.best.Q = TRUE,id = 'Population A')
 res$best.model
 res$best.slope
+
+###TRYING ON MY OWN
+###June 30, 40 deg
+june30_40 <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/June3023_40C.xlsx", range = "A40:CL137") %>%
+  filter(`Time [s]` != "Temp. [°C]") %>% 
+  gather(2:90, key = time, value = OD) %>% 
+  rename(well = `Time [s]`) %>% 
+  mutate(time = as.numeric(time)) %>% 
+  mutate(treatment = case_when(str_detect(well, "A") ~ "fRS585",
+                               str_detect(well, "B") ~ "blank"))
+
+gr_june30_40 <- june30_40 %>% 
+  filter(well == "B2") %>% 
+  mutate(log_od = log(OD)) %>% 
+  mutate(time_days = time / 86400) %>% 
+  view
+
+res<-get.growth.rate(gr_june30_40$time_days, gr_june30_40$log_od,plot.best.Q = TRUE,id = 'Population A')
+res$best.model
+res$best.slope
+
+
+
+
+         
+                               
+
