de055c0db4119261260e8841e419f7e21da0fc76
suzhova <jane@example.com>
2023-07-10 14:37:33 -0400

    made preliminary graph

--- R-scripts/05-thermal-performance-curve.R
+++ R-scripts/05-thermal-performance-curve.R
@@ -1,9 +1,20 @@
 #SVETA
 #PLOTTING THERMAL PERFORMANCE CURVE
 
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(cowplot)
+theme_set(theme_cowplot())
+
 #uploaded the done avg actual temps and calculated growth rates to git in raw data
 #graphing the temps on x, growth rates on y
 
 #READING IN TABLE
-growth_rates_summary_wip <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/growth_rates_summary_wip.xlsx", range = "C1:D7") %>% 
+growth_rates_summary_wip <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/growth_rates_summary_wip.xlsx", range = "C1:D6") %>% 
  view()
+
+grsw_graph <- growth_rates_summary_wip %>% 
+  ggplot(aes(x = `avg actual temp`, y = `avg growth rate`)) + geom_line()
+grsw_graph
