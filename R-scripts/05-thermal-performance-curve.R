fa9fb609d9fd96243469f43341402d76d84bbc28
suzhova <jane@example.com>
2023-07-18 10:52:20 -0400

    graphing with variation

--- R-scripts/05-thermal-performance-curve.R
+++ R-scripts/05-thermal-performance-curve.R
@@ -11,10 +11,34 @@ theme_set(theme_cowplot())
 #uploaded the done avg actual temps and calculated growth rates to git in raw data
 #graphing the temps on x, growth rates on y
 
-#READING IN TABLE
+###GRAPHING AVG GROWTH RATES
+#reading in table
 growth_rates_summary_wip <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/growth_rates_summary_wip.xlsx") %>% 
- view()
+  view()
 
+#code used to graph avg temp and avg growth rate
 grsw_graph <- growth_rates_summary_wip %>% 
-  ggplot(aes(x = `avg actual temp`, y = `avg growth rate`)) + geom_line()
+  ggplot(aes(x = `avg temp`, y = `avg growth rate`)) + geom_line()
 grsw_graph
+
+###GRAPHING WITH VARIATION
+#reading in table
+growth_rates_summary_wip <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/growth_rates_summary_wip.xlsx", sheet = 2, range = "a1:b73") 
+ view(growth_rates_summary_wip)
+
+ grsw_graph2 <- growth_rates_summary_wip %>% 
+   ggplot(aes(x = `temp`, y = `growth rate`)) + geom_point()
+ grsw_graph2
+#multiple issues 
+#how to separate temps by colour
+#how to get rid of so many growth rates - change scales
+#how to add line of curve
+ 
+ growth_rates_summary_wip <- read_excel("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/growth_rates_summary_wip.xlsx", sheet = 3) 
+ view(growth_rates_summary_wip)
+ 
+#code used to graph avg temp and all growth rate data points
+grsw_graph2 <- growth_rates_summary_wip %>% 
+  ggplot(aes(x = "avg temp", y = "growth rate 1")) + geom_point()
+() + geom_errorbar()
+grsw_graph2
