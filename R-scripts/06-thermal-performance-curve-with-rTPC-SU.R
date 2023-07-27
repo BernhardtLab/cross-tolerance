1b17ff1e9d79a676d299f2a9d9c9c0b6bf72a671
suzhova <jane@example.com>
2023-07-27 15:31:26 -0400

    doing all 25 models

--- R-scripts/06-thermal-performance-curve-with-rTPC-SU.R
+++ R-scripts/06-thermal-performance-curve-with-rTPC-SU.R
@@ -13,53 +13,49 @@ library(readxl)
 
 #all models
 get_model_names()
+#[1] "beta_2012"             "boatman_2017"          "briere2_1999"         
+#[4] "delong_2017"           "flinn_1991"            "gaussian_1987"        
+#[7] "hinshelwood_1947"      "joehnk_2008"           "johnsonlewin_1946"    
+#[10] "kamykowski_1985"       "lactin2_1995"          "lrf_1991"             
+#[13] "modifiedgaussian_2006" "oneill_1972"           "pawar_2018"           
 
 #copied briere from personal lab meetings r script
-mod3 = 'briere2_1999'
+d <- read_excel ("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/growth_rates_summary_wip.xlsx", sheet = 2) %>% 
+  rename(rate = `growth rate`)
+view(d)
 
-start_vals_briere <- get_start_vals(d$temp, d$rate, model_name = 'briere2_1999')
-low_lims_briere <- get_lower_lims(d$temp, d$rate, model_name = 'briere2_1999')
-upper_lims_briere <- get_upper_lims(d$temp, d$rate, model_name = 'briere2_1999')
+mod = 'delong_2017'
 
-fit_briere <- nls_multstart(rate~briere2_1999(temp = temp, tmin, tmax, a, b), 
+start_vals <- get_start_vals(d$temp, d$rate, model_name = 'delong_2017')
+start_vals
+low_lims <- get_lower_lims(d$temp, d$rate, model_name = 'delong_2017')
+upper_lims <- get_upper_lims(d$temp, d$rate, model_name = 'delong_2017')
+
+fit_mod <- nls_multstart(rate~delong_2017(temp = temp, c, eb, ef, tm, ehc), 
                             data = d,
                             iter = 500,
-                            start_lower = get_start_vals(d$temp, d$rate, model_name = 'briere2_1999') - 10,
-                            start_upper = get_start_vals(d$temp, d$rate, model_name = 'briere2_1999') + 10,
-                            lower = get_lower_lims(d$temp, d$rate, model_name = 'briere2_1999'),
-                            upper = get_upper_lims(d$temp, d$rate, model_name = 'briere2_1999'),
+                            start_lower = get_start_vals(d$temp, d$rate, model_name = 'delong_2017') - 10,
+                            start_upper = get_start_vals(d$temp, d$rate, model_name = 'delong_2017') + 10,
+                            lower = get_lower_lims(d$temp, d$rate, model_name = 'delong_2017'),
+                            upper = get_upper_lims(d$temp, d$rate, model_name = 'delong_2017'),
                             supp_errors = 'Y')
-fit_briere 
-#Nonlinear regression model
-#model: rate ~ briere2_1999(temp = temp, tmin, tmax, a, b)
-#data: data
-#tmin     tmax        a        b 
-#17.61509 42.15999  0.00372  1.31615 
-#residual sum-of-squares: 326.7
-#Number of iterations to convergence: 42 
-#Achieved convergence tolerance: 1.49e-08
+fit_mod
 
-calc_params(fit_briere) %>%
+calc_params(fit_mod) %>%
   mutate_all(round, 2)
-#WITHOUT 20 AND 30 DEG
-#rmax  topt ctmin ctmax    e   eh    q10 thermal_safety_margin thermal_tolerance breadth skewness
-# 10.22 33.83 17.62 42.09 4.31 1.07 314.61                  8.26             24.48   10.25     3.24
 
-#WITH 30 DEG, WITHOUT 20 DEG
-# rmax  topt ctmin ctmax    e   eh  q10 thermal_safety_margin thermal_tolerance breadth skewness
-# 9.98 33.98  17.6 42.09 0.96 1.07 3.45                  8.11             24.49   10.23    -0.11
-
-new_data_briere <- data.frame(temp = seq(min(d$temp), max(d$temp), 0.5))
-preds_briere <- augment(fit_briere, newdata = new_data)
+new_data <- data.frame(temp = seq(min(d$temp), max(d$temp), 0.5))
+preds <- augment(fit_mod, newdata = new_data)
 
 ggplot(d, aes(temp, rate)) +
   geom_point() +
-  geom_line(aes(temp, .fitted), preds_briere, col = 'pink', size = 2) +
+  geom_line(aes(temp, .fitted), preds, col = 'purple', size = 2) +
   theme_bw(base_size = 12) +
   labs(x = 'Temperature (ºC)',
        y = 'Metabolic rate',
-       title = 'briere')
-AIC(fit, fit_briere)
+       title = 'model')
+
+AIC(fit, fit_mod)
 
 ###SUMMARY WITHOUT 20 AND 30 DEG:
 #fit of sharpeschoolhigh_1981: df = 5, AIC = 330.8485
