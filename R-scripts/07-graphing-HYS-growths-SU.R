08c8a0e515e99645d0080b2a7bbab3922704afe6
suzhova <jane@example.com>
2023-08-18 11:59:52 -0400

    graphed od from day 1

--- R-scripts/07-graphing-HYS-growths-SU.R
+++ R-scripts/07-graphing-HYS-growths-SU.R
@@ -1,2 +1,31 @@
 #SVETA
+
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(cowplot)
+
 #GRAPHING OD READS FROM HYS_1 DAY 1
+hys1_37 <- read_excel ("data-raw/HYS_1/HYS_1.xlsx", sheet = "37") %>% 
+  mutate(temperature = 37)
+view(hys1_37)
+
+#hys1_37 %>% 
+#  ggplot(aes(x = well, y = day1)) + geom_point()
+
+hys1_42  <- read_excel ("data-raw/HYS_1/HYS_1.xlsx", sheet = "42") %>% 
+  mutate(temperature = 42)
+view(hys1_42)
+
+#hys1_42 %>% 
+#  ggplot(aes(x = well, y = day1)) + geom_point()
+
+hys1_both <- bind_rows(hys1_42, hys1_37) %>% 
+  mutate(unique_well = paste(well,temperature, sep = "_")) 
+
+hys1_both %>% 
+  ggplot (aes (x = well, y = day1, colour = factor(temperature), group = unique_well)) + 
+  geom_point() +
+  xlab('well') +
+  ylab('OD600')
