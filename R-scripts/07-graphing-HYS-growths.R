2642df64b8de9bfe2d3d8bf10d3d398075134667
INwafor <inwafor@uoguelph.ca>
2023-09-18 21:15:48 -0400

    added day 4 and 5

--- R-scripts/07-graphing-HYS-growths.R
+++ R-scripts/07-graphing-HYS-growths.R
@@ -386,29 +386,29 @@ library(readxl)
 library(cowplot)
 
 #37C
-sept17_34 <- read_excel ("data-raw/HYS3/HYS3.xlsx", sheet = "34") %>% 
+sept18_34 <- read_excel ("data-raw/HYS3/HYS3.xlsx", sheet = "34") %>% 
   mutate(temperature = 34) %>% 
   mutate(unique_well = paste(well,day,treatment, sep = "_"))
 
-sept17_34 %>% 
+sept18_34 %>% 
   ggplot(aes( x = day, y = OD, colour = treatment, group = unique_well))+
   geom_point(aes(shape = treatment), size = 2) +
   theme_minimal() +
   facet_wrap(~well, scales = "free_x") +
   geom_line(aes(x = day, y = OD, group = well)) +
-  ggtitle('34 days 1-4')
+  ggtitle('34 days 1-5')
 
 #42C
-sept17_41 <- read_excel ("data-raw/HYS3/HYS3.xlsx", sheet = "41") %>% 
+sept18_41 <- read_excel ("data-raw/HYS3/HYS3.xlsx", sheet = "41") %>% 
   mutate(temperature = 41) %>% 
   mutate(unique_well = paste(well,day,treatment, sep = "_"))
 
 
-sept17_41 %>% 
+sept18_41 %>% 
   ggplot(aes( x = day, y = OD, colour = treatment, group = unique_well))+
   geom_point(aes(shape = treatment), size = 2) +
   theme_minimal() +
   facet_wrap(~well, scales = "free_x") +
   geom_line(aes(x = day, y = OD, group = well)) +
-  ggtitle('41 days 1-4')
-#B2 weird --> contaminated? the rest is no growth
+  ggtitle('41 days 1-5')
+#B2 weird --> contaminated
