07c2704ca56f9dc9cf5db8c77dc6aa95c38a6f73
suzhova <jane@example.com>
2024-06-12 16:46:46 -0400

    42 growth curve

--- R-scripts/10-graphing-growth-curves-summer-2024.R
+++ R-scripts/10-graphing-growth-curves-summer-2024.R
@@ -1,1 +1,22 @@
 #growth curves summer 2024
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(cowplot)
+library(janitor)
+
+#June11.2024.42C.Shaking
+trials42 <- read_excel ("C:/Users/sveta/Documents/B Lab/cross-tolerance/data-raw/Growth curves/summer2024/June11.2024.42C.Shaking.xlsx", sheet = "raw") %>% 
+  clean_names()
+View(trials42)
+
+trials42_frs152 <- trials42 %>% 
+  select(c(time, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11))
+View(trials42_frs152)
+
+trials42_frs152 %>% 
+  ggplot(aes(time, b1))+
+  geom_point()
+
+#c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11
