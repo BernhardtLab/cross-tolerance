b554cada0ecf931a0582bc2146dfcbc93462e593
INwafor <inwafor@uoguelph.ca>
2024-10-21 20:44:23 -0400

    cleaned up c.auris data for 43/44C evolved at 35C/43C. Need to add plate layout before graphing for culture/blank

---
+++ R-scripts/11-figure-creation.R
@@ -0,0 +1,118 @@
+#11-figure-creation
+# Ijeoma Nwafor 
+# Oct. 21 2024
+
+library(readxl)
+library(tidyverse)
+library(cowplot)
+theme_set(theme_cowplot())
+library(janitor)
+library(lubridate)
+
+files <- c(list.files("data-raw/Evolution-2024", full.names = TRUE))
+
+# contain the full file paths of all .xlsx files in the folder
+files2 <- files[grepl(".xlsx", files)]
+
+files2 <- files2 %>% 
+  gsub(pattern = ".xlsx$", replacement = "")
+
+#extract all the rep2 sheets
+date_time_list <- list()
+od600_list <- list()
+
+# Loop through each file !! # date and time not entering correctly
+for (file in files2) {
+  # Get all sheet names in the file
+  sheet_names <- excel_sheets(file)
+  
+  # Find sheets that contain "Rep2"
+  rep2_sheets <- sheet_names[grepl("Rep2", sheet_names)]
+  
+  # Loop through matching sheets
+  for (sheet in rep2_sheets) {
+    # Read date and time from A5:B6
+    date_time <- read_excel(file, sheet = sheet, range = "B22", col_names = FALSE)
+    
+    # Read OD600 values from A25:M32
+    od600 <- read_excel(file, sheet = sheet, range = "A25:M32", col_names = FALSE)
+    
+    # Store the extracted date and time
+    date_time_list[[paste(file, sheet, sep = "_")]] <- date_time
+    
+    # Store the extracted OD600 data
+    od600_list[[paste(file, sheet, sep = "_")]] <- od600
+  }
+}
+
+# then pull all individual files into a merged DF by file_name 
+
+all_plates <- map_df(od600_list, ~as.data.frame(.x), .id = "file_name") %>%
+  clean_names() %>%
+  filter(!grepl("<>", x1))
+
+pivot_plates <- all_plates %>%
+  pivot_longer(cols = 3:14, names_to = "well", values_to = "OD600")
+
+#Rename some things
+clean_plates <- pivot_plates %>%
+  rename(row = x1) %>% 
+  mutate(
+    well2 = case_when(
+      well == "x2" ~ 1,
+      well == "x3" ~ 2,
+      well == "x4" ~ 3,
+      well == "x5" ~ 4,
+      well == "x6" ~ 5,
+      well == "x7" ~ 6,
+      well == "x8" ~ 7,
+      well == "x9" ~ 8,
+      well == "x10" ~ 9,
+      well == "x11" ~ 10,
+      well == "x12" ~ 11,
+      well == "x13" ~ 12,
+      TRUE ~ NA_integer_  # Default value if no condition is met
+    )
+  )
+
+# separate file_name by "_", remove well, join row and well, add time 
+# drop brackets and .xlsx from evol_temp
+clean_plates2 <- clean_plates %>%
+  separate(file_name, into = c("data","raw", "evolution","2024", "generation", "evol_temp", "xlsx", "temp", "replicate"), remove = FALSE) %>%
+  select(generation, evol_temp, temp, row, well2, OD600) %>%
+  mutate(well = paste0(row, well2)) %>%  # Create the new `well` column
+  select(-row, -well2)
+
+## now using date_time-list add in the time
+
+all_times <- map_df(date_time_list, ~as.data.frame(.x), .id = "file_name") %>%
+  clean_names() 
+
+head(all_times)
+
+#remove file with "G10+" in it - will come back to this dued to the weird naming (at35C)
+all_times <- all_times %>%
+  filter(!grepl("G10\\+", file_name))
+
+
+all_times2 <- all_times %>%
+  mutate(date_time = as.POSIXct(x1, format = "%Y-%m-%d %I:%M:%S %p")) %>%
+  separate(file_name, into = c("data","raw", "evolution","2024", "generation", "evol_temp", "xlsx", "temp", "replicate"), remove = FALSE) %>% 
+  select(generation, evol_temp, temp, date_time)
+
+alltimes3 <- all_times2 %>%
+  mutate(time_elapsed = round(as.numeric(difftime(date_time, min(date_time), units = "hours"))))
+
+auris_evolution <- left_join(clean_plates2, alltimes3, by = c("generation", "evol_temp" = "temp"))
+## auris csv 
+
+## quick tings <- NOT WORKING NEED PLATE LAYOUT - BLANKS AND CULTURE
+ggplot(auris_evolution, aes(time_elapsed, OD600)) =
+  geom_point(color = )
+
+auris_evolution %>%
+  ggplot(aes(x = time_elapsed, y = OD600, color = evol_temp)) +
+  geom_point()+
+  geom_smooth(method = "loess", se = FALSE) + 
+  facet_wrap(~temp, scales = "free_y") +  
+  theme_minimal()
