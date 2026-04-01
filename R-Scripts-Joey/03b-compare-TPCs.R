5494e563422583e62adc30d0d601b2532d8346f2
Joey Bernhardt <joey.bernhardt@gmail.com>
2026-04-01 15:48:45 -0400

    removing extra packages, formatting of 03b compare

--- R-Scripts-Joey/03b-compare-TPCs.R
+++ R-Scripts-Joey/03b-compare-TPCs.R
@@ -3,15 +3,8 @@
 # load packages -----------------------------------------------------------
 
 library(tidyverse)
-library(cowplot)
-theme_set(theme_cowplot())
-library(lubridate)
-library(growthTools)
 library(rTPC)
 library(minpack.lm)
-library(car)
-library(plotrix)
-library(broom)
 library(conflicted)
 conflict_prefer("select", "dplyr")
 conflicts_prefer(dplyr::filter)
@@ -23,14 +16,12 @@ library(nls.multstart)
 all_blocks_no_lag <- read_csv("data-processed/all-blocks-growth-no-lag.csv")
 
 df <- all_blocks_no_lag |>
-  mutate(curve_id = strain) |>
-  mutate(temp = test_temperature,
-         rate = mu)
+  mutate(curve_id = strain, temp = test_temperature, rate = mu)
 
 length(unique(df$curve_id))
 
 
-# fit both SS and thomas --------------------------------------------------
+# fit TPC models ----------------------------------------------------------
 
 fit_tpc_model <- function(df, model_name, tref = 20, iter = 2000) {
 
@@ -84,32 +75,9 @@ fit_tpc_model <- function(df, model_name, tref = 20, iter = 2000) {
 }
 
 
-fits_both <- df |>
-  group_by(curve_id) |>
-  nest() |>
-  mutate(
-    sharp_high = map(data, ~ fit_tpc_model(.x, "sharpeschoolhigh_1981", tref = 20)),
-    thomas     = map(data, ~ fit_tpc_model(.x, "thomas_2012"))
-  ) |>
-  mutate(
-    AIC_sharp  = map_dbl(sharp_high, "AIC"),
-    AIC_thomas = map_dbl(thomas, "AIC")
-  )
-
-
-fits_both2 <- fits_both |>
-  mutate(
-    best_model = case_when(
-      AIC_sharp < AIC_thomas ~ "sharpeschoolhigh_1981",
-      AIC_thomas < AIC_sharp ~ "thomas_2012",
-      TRUE ~ "tie"
-    )
-  )
-
-# In all cases the SS model is the best model via AIC
-
-
-# now add briere and lactin -----------------------------------------------
+# fit all four models and compare AIC -------------------------------------
+# SS was best in a preliminary SS-vs-Thomas comparison; briere and lactin
+# added here to confirm.
 
 fits_all <- df |>
   group_by(curve_id) |>
